72b5d1ed4c4a01c6b99198d17be1eaae71e5add1
bhagyashreedevopsengineer <bhagyashreedevopsengineer@gmail.com>
2025-07-26 18:26:03 +0530

    initial commit

---
+++ src/main/java/com/example/MyApplication.java
@@ -0,0 +1,25 @@
+package com.example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+
+public class MyApplication {
+
+@RequestMapping("/")
+
+String home(){
+    return "hello world";
+}
+
+@RequestMapping("/welcome")
+String getWelcomeMessage(){
+    return "Welcome To Our Website";
+}
+    public static void main(String args[]){
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
